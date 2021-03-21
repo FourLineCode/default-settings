3ff36feec91d339edabb6e69e94e5c4f1055b592
Akmal Hossain <56719270+FourLineCode@users.noreply.github.com>
2021-03-21 15:12:18 +0600

    first new line removed

--- git-prompt.sh
+++ git-prompt.sh
@@ -10,7 +10,7 @@ then
 	. ~/.config/git/git-prompt.sh
 else
 	PS1='\[\033]0;$TITLEPREFIX:$PWD\007\]' # set window title
-	PS1="$PS1"'\n'                 # new line
+	# PS1="$PS1"'\n'                 # new line
 	PS1="$PS1"'\[\033[32m\]'       # change to green
 	PS1="$PS1"'\u@\h '             # user@host<space>
 	# PS1="$PS1"'\[\033[35m\]'       # change to purple <EDITED>
